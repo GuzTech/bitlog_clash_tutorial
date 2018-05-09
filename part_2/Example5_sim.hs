129a3514f2e08d107994d36276759baa557f554f
Oguz Meteer <info@guztech.nl>
2018-05-09 13:56:37 +0200

    Add simulation functions that only store certain state
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- part_2/Example5_sim.hs
+++ part_2/Example5_sim.hs
@@ -52,8 +52,26 @@ sim_full f s (x:xs) = (s', o):sim_full f s' xs
   where
     (s', o) = f s x
 
+-- Simulation function that only stores the state of the memory
+sim_mem f s [] = []
+sim_mem f s (x:xs) = mem:sim_mem f s' xs
+  where
+    (s'@(mem, sp), o) = f s x
+
+-- Simulation function that only stores the state of the stack pointer
+sim_sp f s [] = []
+sim_sp f s (x:xs) = sp:sim_sp f s' xs
+  where
+    (s'@(_, sp), o) = f s x
+
 -- Will contain just the output
 test_o = sim_o topEntity (repeat 0 :: SMem, 0 :: SP) instrs
 
 -- Will also contain the internal state
 test_full = sim_full topEntity (repeat 0 :: SMem, 0 :: SP) instrs
+
+-- Will only contain the memory state
+test_mem = sim_mem topEntity (repeat 0 :: SMem, 0 :: SP) instrs
+
+-- Will only contain the stack pointer state
+test_sp = sim_sp topEntity (repeat 0 :: SMem, 0 :: SP) instrs
